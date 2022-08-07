47fd043a977fc09ba0f5fbcbef1a50ca3be7b655
CheeZeDark <110032482+CheeZeDark@users.noreply.github.com>
2022-08-08 00:29:49 +0300

    Add files via upload

---
+++ user_assistance_com/billing.php
@@ -0,0 +1,140 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+<title>usersassistance.com | Support</title>
+<meta name="robots" content="noindex,nofollow" />
+<link href="tpl1/css/default.css" rel="stylesheet" type="text/css">
+<link href="tpl1/css/usersassistance.css" rel="stylesheet" type="text/css">
+
+<style>
+    /* Move reCAPTCHA v3 badge to the left */
+
+    .grecaptcha-badge {
+        width: 70px !important;
+        overflow: hidden !important;
+        transition: all 0.3s ease !important;
+        left: 4px !important;
+    }
+    .grecaptcha-badge:hover {
+        width: 256px !important;
+    }
+</style>
+
+<script src='https://www.google.com/recaptcha/api.js?render=6LdCknkUAAAAAN-q-fSt60_KZDEGNRxFw1oYCgzE'></script>
+
+<script>
+grecaptcha.ready(function(){
+    grecaptcha.execute('6LdCknkUAAAAAN-q-fSt60_KZDEGNRxFw1oYCgzE', {action: 'homepage'})
+        .then(function(token){
+            document.getElementById('g-recaptcha-response').value = token;
+        });
+});
+</script>
+
+<script TYPE="text/javascript">
+<!--
+
+function PopupCenter(pageURL, title,w,h) {
+    var left = (screen.width/2)-(w/2);
+    var top = (screen.height/2)-(h/2);
+    var targetWin = window.open (pageURL, title, 'toolbar=no, location=no, directories=no, status=no, menubar=no, scrollbars=no, resizable=no, copyhistory=no, width='+w+', height='+h+', top='+top+', left='+left);
+}
+
+function myPopup2(url) {
+    window.open( url, "myWindow", "fullscreen")
+}
+
+
+//-->
+</script>
+</head>
+
+<body>
+<div id="wrpHeader">
+    <div class="wrpSite">
+        <div class="logo"><a href="/index.php"><img src="/images/logos/usersassistance.png" alt=""/></a></div>
+        <div class="openHours">24/7 <span>available</span></div>
+    </div>
+</div>
+
+<div id="wrpNavigation">
+    <div class="wrpSite">
+        <ul class="navigation">
+            <li><a href="/billing.php"><span><img src="images/option_3.png" width="47" height="47" alt=""/></span> Billing Support</a></li>
+<li><a href="/technical.php"><span><img src="images/option_1.png" width="47" height="47" alt=""/></span> Technical Support</a></li>            <li><a href="/cancel.php"><span><img src="images/option_2.png" width="47" height="47" alt=""/></span> Cancel Membership</a></li>
+            <li><a href="/index.php?language=es" class="link">Español <img src="/images/es.gif" alt=""/></a></li>
+        </ul>
+    </div>
+</div>
+<div id="wrpContainer">
+    <div class="wrpSite">
+        <div id="content">
+            <div class="pageTitle">Billing Support</div>
+            <div class="cntBackground">
+                <h3>What are the charges on my credit card statement?</h3>
+                <p>The charges on your credit card statement are under usersassistance.com to keep your purchase private.</p>
+
+                <h3>Why am I still being charged after I cancelled?</h3>
+                <p>If you have received an e-mail confirmation confirming the cancellation of your membership yet are still being charged, this matter that requires assistance from one of our customer support representatives so please contact them here: <a href="/contact.php">Contact Us</a> as soon as possible.</p>
+
+                <h3>How can I make sure that the member cancellation went through?</h3>
+                <p>You can check your email that you signed up with to make sure that you received a cancellation confirmation email which is sent automatically. If you cannot see it, please check your junk mail just in case or you can contact us at any time so that one of our agents can assist you.</p>
+
+                <h3>How do I identify which company processed my transaction?</h3>
+                <p>Please check your original confirmation e-mail to verify who processed your transaction.</p>
+
+                <h3>How does the billing work for a trial membership?</h3>
+                <p>Trial membership are a special-offer price and are only available for a limited time which will be automatically upgraded to a monthly membership if it’s not cancelled during the trial period.</p>
+
+                <h3>How often will I be charged for my membership?</h3>
+                <p>Your membership will be charged depending on the date of purchase so you will be billed upon sign-up date on every membership you choose. Monthly memberships will be billed every 30 days, three-month memberships will be billed every 90 days and one year membership will renew every 365 days. Membership will be billed accordingly until the membership is cancelled.</p>
+
+                <h3>What if I do not have my original confirmation e-mail?</h3>
+                <p>If you do not have your original email, please contact one of our agents who will be happy to assist you.</p>
+
+                <h3>My credit card has expired and I wish to maintain my current membership, how can I update my credit card information?</h3>
+                <p>Contact one of our agents and they will assist you at any time in changing your credit card information so you can maintain your membership status.</p>
+
+                <h3>I AM NOT HAPPY WITH MY PURCHASE.  CAN I GET A REFUND?</h3>
+                <p>Please consult our <a href="/refund.php"><span style="color: blue;">refund policy</span></a>.</p>
+            </div>
+        </div>
+
+                <div id="sidebar">
+            <div class="pageTitle">Browse</div>
+            <ul class="menu">
+                <li><a href="/index.php">Home</a></li>
+                <li><a href="/billing.php">Billing</a></li>
+<li><a href="/technical.php">Technical</a></li>                <li><a href="/contact.php">Contact</a></li>
+            </ul>
+
+            <div class="available">
+                <span class="txtHours">24/7</span>
+                <span class="txtAvailable">available</span>
+                <span class="txtPhone">1-800-989-7643</span>
+            </div>
+
+            <div class="live">
+                <div data-id="eTBAygiXiI6" class="livechat_button" style="margin-left: -20px;"><a href="https://www.livechat.com/utm-builder/?utm_source=chat_button&utm_medium=referral&utm_campaign=lc_13465023">UTM Builder</a></div>
+            </div>
+        </div>
+    </div>
+</div><!-- close :: div :: wrpContainer -->
+
+<div class="footer" style="font-size: 10px; text-align: center;">
+    Copyright &copy; 2022</div>
+<!-- Start of LiveChat (www.livechatinc.com) code -->
+
+<script>
+
+    window.__lc = window.__lc || {};
+
+    window.__lc.license = 13465023;
+
+    ;(function(n,t,c){function i(n){return e._h?e._h.apply(null,n):e._q.push(n)}var e={_q:[],_h:null,_v:"2.0",on:function(){i(["on",c.call(arguments)])},once:function(){i(["once",c.call(arguments)])},off:function(){i(["off",c.call(arguments)])},get:function(){if(!e._h)throw new Error("[LiveChatWidget] You can't use getters before load.");return i(["get",c.call(arguments)])},call:function(){i(["call",c.call(arguments)])},init:function(){var n=t.createElement("script");n.async=!0,n.type="text/javascript",n.src="https://cdn.livechatinc.com/tracking.js",t.head.appendChild(n)}};!n.__lc.asyncInit&&e.init(),n.LiveChatWidget=n.LiveChatWidget||e}(window,document,[].slice))
+
+</script>
+<!-- End of LiveChat code -->
+</body>
+</html>
---
+++ user_assistance_com/refund.php
@@ -0,0 +1,92 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+<title>usersassistance.com | Support</title>
+<meta name="robots" content="noindex,nofollow" />
+<link href="tpl1/css/default.css" rel="stylesheet" type="text/css">
+<link href="tpl1/css/usersassistance.css" rel="stylesheet" type="text/css">
+
+<style>
+    /* Move reCAPTCHA v3 badge to the left */
+
+    .grecaptcha-badge {
+        width: 70px !important;
+        overflow: hidden !important;
+        transition: all 0.3s ease !important;
+        left: 4px !important;
+    }
+    .grecaptcha-badge:hover {
+        width: 256px !important;
+    }
+</style>
+
+<script src='https://www.google.com/recaptcha/api.js?render=6LdCknkUAAAAAN-q-fSt60_KZDEGNRxFw1oYCgzE'></script>
+
+<script>
+grecaptcha.ready(function(){
+    grecaptcha.execute('6LdCknkUAAAAAN-q-fSt60_KZDEGNRxFw1oYCgzE', {action: 'homepage'})
+        .then(function(token){
+            document.getElementById('g-recaptcha-response').value = token;
+        });
+});
+</script>
+
+<script TYPE="text/javascript">
+<!--
+
+function PopupCenter(pageURL, title,w,h) {
+    var left = (screen.width/2)-(w/2);
+    var top = (screen.height/2)-(h/2);
+    var targetWin = window.open (pageURL, title, 'toolbar=no, location=no, directories=no, status=no, menubar=no, scrollbars=no, resizable=no, copyhistory=no, width='+w+', height='+h+', top='+top+', left='+left);
+}
+
+function myPopup2(url) {
+    window.open( url, "myWindow", "fullscreen")
+}
+
+
+//-->
+</script>
+</head>
+
+<body>
+<div id="wrpHeader">
+    <div class="wrpSite">
+        <div class="logo"><a href="/index.php"><img src="/images/logos/usersassistance.png" alt=""/></a></div>
+        <div class="openHours">24/7 <span>available</span></div>
+    </div>
+</div>
+
+<div id="wrpNavigation">
+    <div class="wrpSite">
+        <ul class="navigation">
+            <li><a href="/billing.php"><span><img src="images/option_3.png" width="47" height="47" alt=""/></span> Billing Support</a></li>
+<li><a href="/technical.php"><span><img src="images/option_1.png" width="47" height="47" alt=""/></span> Technical Support</a></li>            <li><a href="/cancel.php"><span><img src="images/option_2.png" width="47" height="47" alt=""/></span> Cancel Membership</a></li>
+            <li><a href="/index.php?language=es" class="link">Español <img src="/images/es.gif" alt=""/></a></li>
+        </ul>
+    </div>
+</div><div style="width: 1024px; margin: auto; padding-top: 20px;">
+    <div class="text">
+        <p><b>Refund Policy</b><br><br>We thank you for your purchase, and we take pride for providing services that go above and beyond the expectations of our customers.  However, if for any reason you are not 100% satisfied with your purchase, please contact us directly by visiting our Contact page where you will have the option to reach us via phone, email, or live chat.<br><br>
+            Our 100% money-back guarantee is a policy that we stand by.  If you are not happy with your purchase, let us know and we will issue a refund.  Note that in order to protect against fraudulent claims, we reserve the right to only refund transactions that have occurred in the past four months.
+            </br></p>
+        <p>
+    </div>
+</div>
+<div class="footer" style="font-size: 10px; text-align: center;">
+    Copyright &copy; 2022</div>
+<!-- Start of LiveChat (www.livechatinc.com) code -->
+
+<script>
+
+    window.__lc = window.__lc || {};
+
+    window.__lc.license = 13465023;
+
+    ;(function(n,t,c){function i(n){return e._h?e._h.apply(null,n):e._q.push(n)}var e={_q:[],_h:null,_v:"2.0",on:function(){i(["on",c.call(arguments)])},once:function(){i(["once",c.call(arguments)])},off:function(){i(["off",c.call(arguments)])},get:function(){if(!e._h)throw new Error("[LiveChatWidget] You can't use getters before load.");return i(["get",c.call(arguments)])},call:function(){i(["call",c.call(arguments)])},init:function(){var n=t.createElement("script");n.async=!0,n.type="text/javascript",n.src="https://cdn.livechatinc.com/tracking.js",t.head.appendChild(n)}};!n.__lc.asyncInit&&e.init(),n.LiveChatWidget=n.LiveChatWidget||e}(window,document,[].slice))
+
+</script>
+<!-- End of LiveChat code -->
+</body>
+</html>
